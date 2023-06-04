8e54becded405d91cda14a9f6123176111571cc7
antshar <antonshushar@gmail.com>
2023-06-04 22:51:20 +0300

    Card aggregation in Column

--- main/js/structure/Column.js
+++ main/js/structure/Column.js
@@ -1,7 +1,20 @@
+import { Card } from './Card.js';
+
 export class Column {
 	constructor(board, element) {
 		this.board = board;
 		this.element = element;
 
+		const itemElements = element.querySelectorAll('.board__item');
+		this.cards = Array.from(itemElements).map(item => new Card(this, item));
 	}
 
+	putCard(card, afterCard) {
+		if (dropIndex >= this.cards.length) {
+			this.element.appendChild(card.boardRow);
+			return;
+		}
+
+		this.element.insertBefore(card.boardRow, this.cards[dropIndex].boardRow);
+	}
+}
