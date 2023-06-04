1f5d714f99d9d4dab2e5017b79a9059a51a95152
antshar <antonshushar@gmail.com>
2023-06-04 22:51:40 +0300

    added moveCard to Board

--- main/js/structure/Board.js
+++ main/js/structure/Board.js
@@ -1,5 +1,23 @@
+import { Column } from './Column.js';
+
 export class Board {
 	constructor(element) {
 		this.element = element;
 		console.log(element.querySelectorAll('.board__column'));
+		this.columns = Array.from(element.querySelectorAll('.board__column')).map(column => new Column(this, column));
+		this.draggedCard = null;
+		this.targetCard = null;
 	}
+
+	moveCard(cardFrom, cardTo) {
+		const targetColumn = cardTo.column;
+		const cardsAmount = targetColumn.cards.length;
+		const dropIndex = targetColumn.cards.indexOf(cardTo);
+		if (dropIndex >= cardsAmount - 1) {
+			targetColumn.element.appendChild(cardFrom.boardRow);
+			return;
+		} else {
+			targetColumn.element.insertBefore(cardFrom.boardRow, targetColumn.cards[dropIndex + 1].boardRow);
+		}
+	}
+}
